401ae91df43382abfaf321bf2ce6cd7bdfdb1b49
Epiphanus <onyesoepiphanus@gmail.com>
2025-06-28 03:16:25 +0100

    implement interview generation

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyClPVd8QP1aBLCoNe-e8viCYcPbAaQ-hok",
-    authDomain: "prepwise-3d07a.firebaseapp.com",
-    projectId: "prepwise-3d07a",
-    storageBucket: "prepwise-3d07a.firebasestorage.app",
-    messagingSenderId: "172495411470",
-    appId: "1:172495411470:web:b120bf2805e70e0bc31f5f",
-    measurementId: "G-FBDHTLWTQT"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 }
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
